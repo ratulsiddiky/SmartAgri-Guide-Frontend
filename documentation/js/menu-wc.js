1a371e9ca75a9a46b8e8e3781840af0c92beade3
ratulsiddiky <ratulsiddiky123@gmail.com>
2026-03-24 03:47:06 +0000

    Documentaion

---
+++ documentation/js/menu-wc.js
@@ -0,0 +1,226 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">smart-agri-fe documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AdminDashboard.html" data-type="entity-link" >AdminDashboard</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Businesses.html" data-type="entity-link" >Businesses</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FarmDetail.html" data-type="entity-link" >FarmDetail</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FarmForm.html" data-type="entity-link" >FarmForm</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FarmsList.html" data-type="entity-link" >FarmsList</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/GlobalAlert.html" data-type="entity-link" >GlobalAlert</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Home.html" data-type="entity-link" >Home</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Login.html" data-type="entity-link" >Login</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Register.html" data-type="entity-link" >Register</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#directives-links"' :
+                                'data-bs-target="#xs-directives-links"' }>
+                                <span class="icon ion-md-code-working"></span>
+                                <span>Directives</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="directives-links"' : 'id="xs-directives-links"' }>
+                                <li class="link">
+                                    <a href="directives/HighlightStatusDirective.html" data-type="entity-link" >HighlightStatusDirective</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/ApiService.html" data-type="entity-link" >ApiService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/AuthService.html" data-type="entity-link" >AuthService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/FarmService.html" data-type="entity-link" >FarmService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/NotificationService.html" data-type="entity-link" >NotificationService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/AuthResponse.html" data-type="entity-link" >AuthResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/BroadcastAlertRequest.html" data-type="entity-link" >BroadcastAlertRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/BroadcastAlertResponse.html" data-type="entity-link" >BroadcastAlertResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Farm.html" data-type="entity-link" >Farm</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmAddress.html" data-type="entity-link" >FarmAddress</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmAlert.html" data-type="entity-link" >FarmAlert</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmInsights.html" data-type="entity-link" >FarmInsights</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmListResponse.html" data-type="entity-link" >FarmListResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmListResult.html" data-type="entity-link" >FarmListResult</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmMutationResponse.html" data-type="entity-link" >FarmMutationResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmPagination.html" data-type="entity-link" >FarmPagination</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/FarmSensor.html" data-type="entity-link" >FarmSensor</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IrrigationStatus.html" data-type="entity-link" >IrrigationStatus</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/NotificationState.html" data-type="entity-link" >NotificationState</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/RegionalInsightsResponse.html" data-type="entity-link" >RegionalInsightsResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/RegisterRequest.html" data-type="entity-link" >RegisterRequest</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/RegisterResponse.html" data-type="entity-link" >RegisterResponse</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/SensorReading.html" data-type="entity-link" >SensorReading</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/User.html" data-type="entity-link" >User</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
